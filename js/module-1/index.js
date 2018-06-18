ce9dae5813ccad6d5c8d8e601d6a41e3f28373e8
Yegor Ivanovskyy <georg.polski@gmail.com>
2018-06-18 23:26:58 +0300

    module-1 done

--- js/module-1/index.js
+++ js/module-1/index.js
@@ -1,4 +1,24 @@
 'use strict';
-let name;
-name = 'Mango';
-console.log( name );
+let message;
+const ADMIN_LOGIN = 'admin';
+const ADMIN_PASSWORD = 'm4ngo1zh4ackz0r';
+const messageBanned = 'Доступ запрещён!';
+const messageDeclined ='Отменено пользователем';
+
+let userLogin = prompt('Введите Ваш логин');
+
+if (userLogin === null){
+    message = alert(messageDeclined);
+}
+else if (userLogin === ADMIN_LOGIN){
+let userPassword = prompt('Введите Ваш пароль');
+if (userPassword === null){
+    message = alert(messageDeclined);
+} else if(userPassword === ADMIN_PASSWORD){
+    message = alert('Добро Пожаловать!');
+} else{
+    message =  alert(messageBanned);
+}
+} else {
+    message =  alert(messageBanned);
+}
