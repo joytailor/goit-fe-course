d70d6ff74dd04bcabd240a79eb1f70905d7da7f5
Yegor Ivanovskyy <georg.polski@gmail.com>
2018-07-04 16:56:29 +0300

    module-3

--- js/module-3/index.js
+++ js/module-3/index.js
@@ -1,12 +1,17 @@
 'use strict';
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+let login = prompt('login pls');
 const checkLoginValidity = function(login){
-if(login.length >= 4 && login.length <= 16){
-    return true;
-} return false;
+    return login.length >= 4 && login.length <= 16
 };
 const checkIfLoginExists = function(logins,login){
- if(logins.includes(login)){
-     return true;
- } return false;
-};
+    return logins.includes(login)
+    };
+const addLogin = function(logins,login){
+if(checkLoginValidity = false){ break;
+console.log('Ошибка! Логин должен быть от 4 до 16 символов');
+} else if(checkIfLoginExists = false){
+    login.push(logins)
+    console.log('Логин успешно добавлен!');
+} else {console.log('Такой логин уже используется!');}
+}
