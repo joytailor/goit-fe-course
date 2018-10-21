2d0634f136d8fb23d49416bb5a5adc0810401c6d
Yegor <georg.polski@gmail.com>
2018-10-21 22:00:45 +0300

    module-14 fixed

--- js/module-14/src/tests/model.test.js
+++ js/module-14/src/tests/model.test.js
@@ -1,3 +1,4 @@
+
 import  Model  from '../model';
 
 describe('Model class', () =>{
@@ -8,24 +9,27 @@ describe('Model class', () =>{
         const model = new Model();
         expect(model instanceof Model).toBe(true);
     })
-    it(`Should check if item's available`), ()=>{
-        const item = checkItem("https://www.youtube.com/watch?v=OPf0YbXqDm0")
+    it(`Should check if item's available`, ()=>{
+        const model = new Model(["https://www.youtube.com/watch?v=OPf0YbXqDm0", "https://app.schoology.com/course/1606293611/student_grades"]);
+        const item = model.checkItem("https://www.youtube.com/watch?v=OPf0YbXqDm0");
         expect(item).toBeNull();
-    }
-    it(`Should check if item isn't available`), ()=>{
-        const item = checkItem("https://jestjs.io/docs/en/using-matchers")
+    })
+    it(`Should check if item isn't available`, ()=>{
+        const model = new Model();
+        const item = model.checkItem("https://jestjs.io/docs/en/using-matchers")
         expect(item).toBeDefined();
-    }
-    it(`Should check if url is valid`), ()=>{
-        const item = checkURL("https://jestjs.io/docs/en/using-matchers")
+    })
+    it(`Should check if url is valid`, ()=>{
+        const model = new Model();
+        const item = model.checkURL("https://jestjs.io/docs/en/using-matchers")
         expect(item).toBeTruthy();
-    }
-    it(`Should check if url isn't valid`), ()=>{
-        const item = checkURL("123")
+    })
+    it(`Should check if url isn't valid`, ()=>{
+      const model = new Model();
+        const item = model.checkURL("123")
         expect(item).toBeFalsy();
-    }
+    })
 })
-
 
 // Should get items from l.s
 // Should push item to l.s.
